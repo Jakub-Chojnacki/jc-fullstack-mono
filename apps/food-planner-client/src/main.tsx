9255b1db2e9838e830856752f9fa33c9f23a3984
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2025-04-24 11:29:43 +0200

    fix: remove redeclaration of tanstack Register type

--- apps/food-planner-client/src/main.tsx
+++ apps/food-planner-client/src/main.tsx
@@ -5,16 +5,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-import { routeTree } from "./routeTree.gen";
 import "./index.css";
+import { routeTree } from "./routeTree.gen";
 
 export const router = createRouter({ routeTree });
-
-declare module "@tanstack/react-router" {
-  type Register = {
-    router: typeof router;
-  };
-}
 
 export type SinglePath = keyof (typeof router)["routesByPath"];
 
