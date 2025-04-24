45f67ccbead155e7722b1aa2c4f77c01bdc4e4cd
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2025-04-24 21:37:29 +0200

    refactor: fix builds by generating commonJS for backend and ESM for frontend in api-contract

--- apps/food-planner-client/vite.config.ts
+++ apps/food-planner-client/vite.config.ts
@@ -1,43 +1,48 @@
 import path from "node:path";
 
-import commonjs from "@rollup/plugin-commonjs";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    react(),
-    tsconfigPaths(),
-  ],
-  build: {
-    rollupOptions: {
-      plugins: [commonjs()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const API_TARGET = `${env.VITE_API_TARGET ?? "http://localhost:3000"}`;
+
+  return ({
+    plugins: [
+      TanStackRouterVite({ autoCodeSplitting: true }),
+      react(),
+      tsconfigPaths(),
+    ],
+    build: {
+
     },
-  },
-  optimizeDeps: {
-    include: ["@jcmono/api-contract/**/*"],
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@hooks": path.resolve(__dirname, "./src/hooks"),
-      "@utils": path.resolve(__dirname, "./src/utils"),
-      "@forms": path.resolve(__dirname, "./src/forms"),
-      "@queries": path.resolve(__dirname, "./src/queries"),
-      "@layouts": path.resolve(__dirname, "./src/layouts"),
+    optimizeDeps: {
+      include: ["@jcmono/api-contract/**/*"],
     },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@components": path.resolve(__dirname, "./src/components"),
+        "@hooks": path.resolve(__dirname, "./src/hooks"),
+        "@utils": path.resolve(__dirname, "./src/utils"),
+        "@forms": path.resolve(__dirname, "./src/forms"),
+        "@queries": path.resolve(__dirname, "./src/queries"),
+        "@layouts": path.resolve(__dirname, "./src/layouts"),
       },
     },
-  },
-});
+
+    server: {
+      proxy: {
+        "/api": {
+          target: API_TARGET,
+          changeOrigin: true,
+        },
+      },
+    },
+  });
+},
+);
--- packages/api-contract/dist/schemas/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./auth";
-export * from "./ingredients";
-export * from './recipes';
-export * from './utils';
-export * from './scheduleMeals';
-export * from './shoppingListIngredients';
