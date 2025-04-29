b00f135542c32f840f104825a8a76e457d6df429
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2025-04-29 10:37:14 +0200

    feat: add queries fro shoppingListIngredients

---
+++ apps/food-planner-client/src/queries/useDeleteShoppingListIngredient/index.tsx
@@ -0,0 +1,20 @@
+import toast from "react-hot-toast";
+
+import apiClient from "@/api-client";
+import { queryClient } from "@/main";
+
+function useDeleteShoppingListIngredient() {
+  const mutation = apiClient.scheduleMeals.delete.useMutation({
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["shoppingListIngredients"] });
+      toast.success("Ingredient deleted successfully!");
+    },
+    onError: () => {
+      toast.error("Error deleting ingredient from the list.");
+    },
+  });
+
+  return mutation;
+}
+
+export default useDeleteShoppingListIngredient;
