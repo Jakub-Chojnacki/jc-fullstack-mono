4e72e4240e75c9caac02c553cf48ab000a16e80b
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2025-05-08 19:25:19 +0200

    feat: create Dockerfiles, make changes to fix an infinite loop when there is a CORS error

--- apps/food-planner-client/vite.config.ts
+++ apps/food-planner-client/vite.config.ts
@@ -9,7 +9,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
-  const API_TARGET = `${env.VITE_API_TARGET ?? "http://localhost:3000"}`;
+  const API_TARGET = `${env.VITE_API_BASE_URL ?? "http://localhost:3000"}`;
 
   return ({
     plugins: [
