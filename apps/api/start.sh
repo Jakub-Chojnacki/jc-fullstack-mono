637851d728d548e6ebdd50dc4eee1192d15bfebd
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2025-05-12 13:25:20 +0200

    refactor: remove "waiting for database" from start.sh

--- apps/api/start.sh
+++ apps/api/start.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-
-echo "⏳ Waiting for database..."
-until nc -z -v -w30 db-hostname 5432; do
-  echo "Waiting for Postgres to be ready..."
-  sleep 2
-done
-
 echo "🚀 Running Prisma Migrate Deploy..."
 npx prisma migrate deploy
 
